276bca3f267734c8faab883a8b1c63dac13ab930
Nicolas Cavigneaux <nico@bounga.org>
2013-08-13 11:17:08 +0200

    Add grey background and black header

--- app/controllers/task_view_controller.rb
+++ app/controllers/task_view_controller.rb
@@ -3,7 +3,20 @@ class TaskViewController < UIViewController
   private
 
   def loadView
+    layoutView
+  end
+
+  def layoutView
+    # Initialisation de la vue
     self.view = UIView.alloc.init
-    self.view.backgroundColor = UIColor.redColor
+
+    # Utilisation d'un gris léger en couleur de fond
+    self.view.backgroundColor = UIColor.colorWithRed(0.902, green: 0.902, blue: 0.902, alpha: 1.0)
+
+    # Ajout d'un en-tête
+    @headerImageView = UIImageView.alloc.initWithFrame([[0, 0], [320, 60]])
+    @headerImageView.image = UIImage.imageNamed("bgHeader.png")
+
+    self.view.addSubview(@headerImageView)
   end
 end
