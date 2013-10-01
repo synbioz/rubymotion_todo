bf78e5ce2d3aeebb4d9a2a002888cff1b3fb593a
Nicolas Cavigneaux <nico@bounga.org>
2013-10-01 15:54:19 +0200

    Add tableView header

--- app/controllers/list_view_controller.rb
+++ app/controllers/list_view_controller.rb
@@ -17,6 +17,28 @@ class ListViewController < UITableViewController
     cell
   end
 
+  def tableView(tableView, viewForHeaderInSection:section)
+    # Ajout d'un en-tête
+    headerImageView = UIImageView.alloc.initWithFrame([[0, 0], [320, 60]])
+    headerImageView.image = UIImage.imageNamed("bgHeader.png")
+
+    # Ajout d'un titre à l'image d'en-tête
+    headerTitle = UILabel.alloc.initWithFrame([[0, 0], [320, 50]])
+    headerTitle.text = "RubyMotion Todo"
+    headerTitle.color = UIColor.colorWithRed(0.702, green: 0.702, blue: 0.702, alpha: 1.000)
+    headerTitle.backgroundColor = UIColor.clearColor
+    headerTitle.textAlignment = UITextAlignmentCenter
+    headerTitle.font = UIFont.fontWithName("AvenirNext-Bold", size: 25)
+
+    headerImageView.addSubview(headerTitle)
+
+    headerImageView
+  end
+
+  def tableView(tableView, heightForHeaderInSection:section)
+    60
+  end
+
   private
 
   def loadTodos
