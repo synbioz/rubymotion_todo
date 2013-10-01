7cf07001ae5af71276ac65eea857475d5e52c82f
Nicolas Cavigneaux <nico@bounga.org>
2013-10-01 17:46:43 +0200

    Add back button

--- app/controllers/list_view_controller.rb
+++ app/controllers/list_view_controller.rb
@@ -32,6 +32,7 @@ class ListViewController < UITableViewController
 
     headerImageView.addSubview(headerTitle)
     headerImageView.addSubview(deleteButton)
+    headerImageView.addSubview(backButton)
     headerImageView.setUserInteractionEnabled(true)
 
     headerImageView
@@ -55,6 +56,16 @@ class ListViewController < UITableViewController
     end
   end
 
+  def goBack
+    @taskViewController = TaskViewController.alloc.init
+    @taskViewController.view.frame = self.view.frame
+    UIView.transitionFromView(self.view,
+                              toView: @taskViewController.view,
+                              duration: 0.5,
+                              options: UIViewAnimationOptionTransitionCurlDown,
+                              completion: nil)
+  end
+
   private
 
   def loadTodos
@@ -69,6 +80,19 @@ class ListViewController < UITableViewController
     button.addTarget(self,
       action:"deleteSelectedCell",
       forControlEvents:UIControlEventTouchUpInside)
+
+    button
+  end
+
+  def backButton
+    button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    button.frame = [[270, 0], [50, 50]]
+
+    button.setTitle("<-", forState: UIControlStateNormal)
+
+    button.addTarget(self,
+                    action: "goBack",
+                    forControlEvents: UIControlEventTouchUpInside)
 
     button
   end
