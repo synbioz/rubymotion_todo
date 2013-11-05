c82f17114ab898f1bf6fa8d4d3f4e32f9dbd4b6a
Nicolas Cavigneaux <nico@bounga.org>
2013-11-05 12:53:51 +0100

    Add title to cell

--- app/views/todo_table_view_cell.rb
+++ app/views/todo_table_view_cell.rb
@@ -17,5 +17,19 @@ class TodoTableViewCell < UITableViewCell
       @background_image = UIImageView.alloc.initWithImage(UIImage.imageNamed("bgCell"))
       self.addSubview(@background_image)
     end
+
+    @titleLabel = titleLabel if @titleLabel.nil?
+    @titleLabel.text = @title
+
+    self.addSubview(@titleLabel)
+  end
+
+  def titleLabel
+    titleLabel = UILabel.alloc.initWithFrame([[10, 0], [300, 40]])
+    titleLabel.font = UIFont.fontWithName("AmericanTypewriter-Bold", size: 18)
+    titleLabel.textColor = UIColor.blueColor
+    titleLabel.adjustsFontSizeToFitWidth = true
+    titleLabel.backgroundColor = UIColor.clearColor
+    titleLabel
   end
 end
